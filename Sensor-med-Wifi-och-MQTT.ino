e55ab576c4e0ee5a7e6d8c53ad20c6e54db5dba3
julia-linnea <julia.l.danielsson@gmail.com>
2025-12-10 20:17:55 +0100

    Add files via upload
    Lyckades fixa så att den skickar sensordatan till en server!

---
+++ Sensor-med-Wifi-och-MQTT.ino
@@ -0,0 +1,76 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+const char* ssid = "WIFI";
+const char* password = "LÖSENORD";
+
+// Replace with the IP of your Mosquitto server (not "localhost")
+const char* mqtt_server = "IP-address till datorn som kör MQTT";   
+const int mqtt_port = 1883;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+const int dry = 703; // value for dry sensor
+const int wet = 300; // value for wet sensor
+
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    
+    if (client.connect("ESP8266_SoilSensor")) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" retrying in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
+void setup() { 
+  Serial.begin(9600);
+  setup_wifi();
+  client.setServer(mqtt_server, mqtt_port);
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  int sensorVal = analogRead(A0);
+  int percentageHumidity = map(sensorVal, wet, dry, 100, 0);
+
+  char payload[10];
+  snprintf(payload, sizeof(payload), "%d", percentageHumidity);
+
+  client.publish("grupp3/soil/humidity", payload);
+
+  Serial.print("Sent: ");
+  Serial.print(payload);
+  Serial.println("%");
+
+  delay(1000);
+}
